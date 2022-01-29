1aa83d24a0cee4e7be1f61999d25ec5cb7f7433d
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-01-29 02:25:47 -0500

    write blogRouter, define get all and post routes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 const middleware = require('./utils/middleware');
@@ -21,5 +22,7 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
