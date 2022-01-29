a19ff6ab0aee351bfe3cfe73bb0982ffde902b14
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-01-29 01:35:57 -0500

    write base app.js file

---
+++ app/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('module');
+const cors = require('cors');
+const config = require('./utils/config');
+
+const app = express();
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+logger.info('Connecting to: ', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB: ', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+module.exports = app;
