cc1e24197dc972666bd6d5bb9122f2c80f12d259
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-01-30 02:15:14 -0500

    write list_helper.js for helper functions

---
+++ app/utils/list_helper.js
@@ -0,0 +1,56 @@
+/* eslint-disable max-len */
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => blogs.reduce(((prev, curr) => prev + curr.likes), 0);
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  const max = blogs.reduce((prev, curr) => (prev.likes > curr.likes ? prev : curr));
+
+  return max;
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorCount = {};
+  blogs.forEach((blog) => { authorCount[blog.author] = (authorCount[blog.author] ?? 0) + 1; });
+
+  const maxAuthorKey = Object.keys(authorCount).reduce((prevAuthor, currAuthor) => (authorCount[prevAuthor] > authorCount[currAuthor] ? prevAuthor : currAuthor));
+
+  return {
+    author: maxAuthorKey,
+    blogs: authorCount[maxAuthorKey],
+  };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authorLikeCount = {};
+  blogs.forEach((blog) => {
+    authorLikeCount[blog.author] = (authorLikeCount[blog.author] ?? 0) + blog.likes;
+  });
+
+  const maxAuthorKey = Object.keys(authorLikeCount).reduce((prevAuthor, currAuthor) => (authorLikeCount[prevAuthor] > authorLikeCount[currAuthor] ? prevAuthor : currAuthor));
+
+  return {
+    author: maxAuthorKey,
+    likes: authorLikeCount[maxAuthorKey],
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+
+};
