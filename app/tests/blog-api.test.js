7a8a4897ba89117659c98159167f16541e0302c1
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-01-31 23:41:11 -0500

    more tests for blog api

--- app/tests/blog-api.test.js
+++ app/tests/blog-api.test.js
@@ -8,7 +8,7 @@ const api = supertest(app);
 
 beforeAll(() => testDB.connect());
 beforeEach(async () => {
-  testDB.clear();
+  await testDB.clear();
 
   const blogObjects = testHelper.initialBlogs.map((blog) => new Blog(blog));
   const promiseArray = blogObjects.map((blog) => blog.save());
@@ -21,6 +21,11 @@ test('blogs are returned as JSON', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+test('unique identifier of blog is named id', async () => {
+  const currentBlogs = await testHelper.blogsInDB();
+  currentBlogs.forEach((blog) => expect(blog.id).toBeDefined());
 });
 
 test('all blogs are returned', async () => {
@@ -51,7 +56,7 @@ test('can post a valid blog', async () => {
   expect(titles).toHaveLength(testHelper.initialBlogs.length + 1);
 });
 
-test('cannot post blog invalid blog', async () => {
+test('cannot post blog with missing url', async () => {
   const invalidNote = {
     title: 'supervalidnote',
     author: 'supervalidauthor',
@@ -66,4 +71,35 @@ test('cannot post blog invalid blog', async () => {
   const currentBlogs = await testHelper.blogsInDB();
 
   expect(currentBlogs).toHaveLength(testHelper.initialBlogs.length);
+});
+
+test('cannot post blog with missing title', async () => {
+  const invalidNote = {
+    author: 'supervalidauthor',
+    url: 'supervalidurl.com',
+    likes: 10,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(invalidNote)
+    .expect(400);
+
+  const currentBlogs = await testHelper.blogsInDB();
+
+  expect(currentBlogs).toHaveLength(testHelper.initialBlogs.length);
+});
+
+test('blog posted with no like property is set to 0 likes', async () => {
+  const validNote = {
+    title: 'supervalidnote',
+    author: 'supervalidauthor',
+    url: 'supervalidurl.com',
+  };
+  const response = await api
+    .post('/api/blogs')
+    .send(validNote);
+
+  const savedNote = response.body;
+  expect(savedNote.likes).toBe(0);
 });
