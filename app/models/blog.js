7e05968a6942d7a4dbdd6756cb9871af6b449f5b
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-01-31 23:40:51 -0500

    change _id to id in blog model

--- app/models/blog.js
+++ app/models/blog.js
@@ -1,13 +1,26 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
-  title: String,
+  title: {
+    type: String,
+    required: [true, 'Blog must have a title!'],
+  },
   author: String,
   url: {
     type: String,
     required: [true, 'Link a URL to this blog!'],
   },
   likes: Number,
+});
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
