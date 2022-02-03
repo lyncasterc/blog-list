3fdd1238dd696f849afd4c39ecee8e5048c15a58
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-02 22:54:20 -0500

    use usersRouter in app.js

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 const app = express();
 const middleware = require('./utils/middleware');
@@ -26,5 +27,6 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(middleware.errorHandler);
 module.exports = app;
