22d8bd5b3b7c552eacb26b4f2881289ab81ca5fb
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-11 01:20:26 -0500

    add testing route for cypress tests to reset db

--- app/app.js
+++ app/app.js
@@ -6,6 +6,7 @@ const testDB = require('./tests/test-db');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 
 const app = express();
 const middleware = require('./utils/middleware');
@@ -21,7 +22,9 @@ if (process.env.NODE_ENV === 'production') {
     .catch((error) => {
       logger.error('Error connecting to MongoDB: ', error.message);
     });
-} else if (process.env.NODE_ENV === 'development') {
+// } else if (process.env.NODE_ENV === 'development') {
+//   testDB.connect();
+} else {
   testDB.connect();
 }
 
@@ -34,5 +37,8 @@ app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 app.use(middleware.errorHandler);
 module.exports = app;
