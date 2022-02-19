74350f9894f408520d294374d95e111c1e2ac221
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-19 00:41:31 -0500

    render blogs list in Blogs component

---
+++ frontend/src/components/Blogs.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-alert */
+import React from 'react';
+import propTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import Blog from './Blog';
+import { likeBlog, destroyBlog } from '../reducers/blogReducer';
+import { setFlashMessage } from '../reducers/flashMessageReducer';
+
+function Blogs({ currentUser }) {
+  const dispatch = useDispatch();
+  const blogs = useSelector((state) => [...state.blogs].sort((a, b) => b.likes - a.likes));
+
+  const handleDestroyBlog = (title, id) => {
+    if (window.confirm(`Delete blog "${title}"?`)) {
+      dispatch(destroyBlog(id));
+      dispatch(setFlashMessage({ type: 'success', message: 'Blog deleted!' }, 5));
+    }
+  };
+
+  return (
+    <>
+      {
+        blogs.map((blog) => (
+          <Blog
+            title={blog.title}
+            author={blog.author}
+            likes={blog.likes}
+            url={blog.url}
+            destroyBlog={() => handleDestroyBlog(blog.title, blog.id)}
+            updateLikes={() => dispatch((likeBlog(blog)))}
+            creator={blog.creator.username}
+            currentUser={currentUser}
+            key={blog.id}
+          />
+        ))
+      }
+    </>
+  );
+}
+
+Blogs.propTypes = {
+  currentUser: propTypes.string.isRequired,
+};
+
+export default Blogs;
