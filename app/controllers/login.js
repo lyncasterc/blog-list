b2bf2cbfd3d828c555fafd67213b085724dd60f8
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-19 18:38:12 -0500

    catch mongoose query error in login post route

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,30 +5,34 @@ const User = require('../models/user');
 
 loginRouter.post('/', async (request, response) => {
   const { body } = request;
-  const user = await User.findOne({ username: body.username });
+  try {
+    const user = await User.findOne({ username: body.username });
 
-  const userValidated = !user ? false : await bcrypt.compare(body.password, user.passwordHash);
+    const userValidated = !user ? false : await bcrypt.compare(body.password, user.passwordHash);
 
-  if (!userValidated) {
-    return response.status(401).send({
-      error: 'invalid username or password',
-    });
+    if (!userValidated) {
+      return response.status(401).send({
+        error: 'invalid username or password',
+      });
+    }
+
+    const tokenUserInfo = {
+      username: user.username,
+      id: user.id,
+    };
+
+    const token = jwt.sign(
+      tokenUserInfo,
+      process.env.SECRET = 'scrt',
+      { expiresIn: 60 * 60 },
+    );
+
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name });
+  } catch (error) {
+    response.status(500).send({ error: 'Oops! Something went wrong on our end! Try again.' });
   }
-
-  const tokenUserInfo = {
-    username: user.username,
-    id: user.id,
-  };
-
-  const token = jwt.sign(
-    tokenUserInfo,
-    process.env.SECRET = 'scrt',
-    { expiresIn: 60 * 60 },
-  );
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
