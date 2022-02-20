359c4a0931e1850e221c2a3afbdf524935137102
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-20 01:06:37 -0500

    create navbar component

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logoutUser } from '../reducers/currentUserReducer';
+import navbarStyles from './Navbar.module.css';
+import Button from './Button';
+
+function Navbar() {
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser);
+
+  return (
+    <div className={navbarStyles.navbar}>
+      <Link to="/blogs"> Blogs </Link>
+      <Link to="/users"> Users </Link>
+      <p>
+        {' '}
+        { currentUser.name }
+        {' '}
+        logged in
+        {' '}
+      </p>
+      <Button
+        onClick={() => dispatch(logoutUser())}
+        buttonText="Log out"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
