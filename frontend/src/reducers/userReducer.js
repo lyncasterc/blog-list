a1705b9a088943744d42026ff4524e3c0b055857
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-20 01:05:14 -0500

    create users reducer and store it in store

---
+++ frontend/src/reducers/userReducer.js
@@ -0,0 +1,28 @@
+/* eslint-disable arrow-body-style */
+import { createSlice } from '@reduxjs/toolkit';
+import userService from '../services/users';
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState: [],
+  reducers: {
+    setUsers(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export default userSlice.reducer;
+
+export const { setUsers } = userSlice.actions;
+
+export const initalizeUsers = () => {
+  return async (dispatch) => {
+    try {
+      const users = await userService.getAll();
+      dispatch(setUsers(users));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import blogReducer from './reducers/blogReducer';
 import flashMessageReducer from './reducers/flashMessageReducer';
 import currentUserReducer from './reducers/currentUserReducer';
+import userReducer from './reducers/userReducer';
 
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
+    users: userReducer,
     flashMessages: flashMessageReducer,
     currentUser: currentUserReducer,
   },
