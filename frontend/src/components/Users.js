0e3503b9c56042c3565e306ae1b1544cb48f1495
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-20 16:13:51 -0500

    use redux thunk to fetch all users in Users component

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { initializeUsers } from '../reducers/userReducer';
 import usersStyles from './Users.module.css';
 
 function Users() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, [dispatch]);
+
   const users = useSelector((state) => [...state.users].sort((a, b) => a.name - b.name));
 
   return (
-    <>
+    <div className="container">
       <h2> Users </h2>
 
-      <div>
+      <div className={usersStyles['users-list']}>
         <h3 className={usersStyles['blogs-created']}> blogs created </h3>
 
         {
         users.map((user) => (
-          <div key={user.id} className={usersStyles['user-item']}>
+          <div
+            key={user.id}
+            className={`${usersStyles['user-item']} flex`}
+            data-cy="user-item"
+          >
             <p>{user.name}</p>
             {' '}
             <p>{user.blogs.length}</p>
@@ -22,7 +32,7 @@ function Users() {
         ))
       }
       </div>
-    </>
+    </div>
   );
 }
 
