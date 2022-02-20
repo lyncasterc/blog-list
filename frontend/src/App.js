7e320d6446c41e1d56e030b205e09da2c79b4a68
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-19 19:01:17 -0500

    pull all loginform related state/funcs from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-alert */
 import './App.css';
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import loginService from './services/login';
-import blogService from './services/blogs';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { initalizeBlogs } from './reducers/blogReducer';
-import { setFlashMessage } from './reducers/flashMessageReducer';
+import { initalizeCurrentUser, logoutUser } from './reducers/currentUserReducer';
 import LoginForm from './components/LoginForm';
 import Blogs from './components/Blogs';
 import Button from './components/Button';
@@ -15,62 +12,33 @@ import Togglable from './components/Togglable';
 
 function App() {
   const dispatch = useDispatch();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [userTokenInfo, setUserTokenInfo] = useState(null);
+  const currentUser = useSelector((state) => state.currentUser);
 
   useEffect(() => {
     dispatch(initalizeBlogs());
   }, [dispatch]);
 
   useEffect(() => {
-    const JSONTokenInfo = localStorage.getItem('bloglistAppUser');
-    if (JSONTokenInfo) {
-      const parsedTokenInfo = JSON.parse(JSONTokenInfo);
-      blogService.setToken(parsedTokenInfo.token);
-      setUserTokenInfo(parsedTokenInfo);
-    }
-  }, []);
+    dispatch(initalizeCurrentUser());
+  }, [dispatch]);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const tokenInfo = await loginService.login({ username, password });
-      if (tokenInfo) {
-        localStorage.setItem('bloglistAppUser', JSON.stringify(tokenInfo));
-        blogService.setToken(tokenInfo.token);
-        setUserTokenInfo(tokenInfo);
-      }
-
-      setUsername('');
-      setPassword('');
-    } catch (error) {
-      dispatch(setFlashMessage({ type: 'error', message: error.message }, 5));
-    }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('bloglistAppUser');
-    setUserTokenInfo(null);
-  };
-
-  if (userTokenInfo) {
+  if (currentUser.token) {
     return (
       <div>
         <FlashMessage />
         <p>
           Hello,
           {' '}
-          {userTokenInfo.name}
+          {currentUser.name}
           {' '}
           <Button
             buttonText="Log out"
-            onClick={handleLogout}
+            onClick={() => dispatch(logoutUser())}
           />
         </p>
 
         <Blogs
-          currentUser={userTokenInfo.username}
+          currentUser={currentUser.username}
         />
 
         <h2> Add New Blog </h2>
@@ -88,13 +56,7 @@ function App() {
       <FlashMessage />
       <h2> Log In </h2>
 
-      <LoginForm
-        handleLogin={handleLogin}
-        username={username}
-        password={password}
-        setUsername={({ target }) => setUsername(target.value)}
-        setPassword={({ target }) => setPassword(target.value)}
-      />
+      <LoginForm />
 
     </div>
   );
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import propTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setFlashMessage } from '../reducers/flashMessageReducer';
+import { loginUser } from '../reducers/currentUserReducer';
 
-function LoginForm({
-  handleLogin, username, password, setUsername, setPassword,
-}) {
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(loginUser(username, password));
+    } catch (error) {
+      dispatch(setFlashMessage({ type: 'error', message: error.message }, 10));
+      setUsername('');
+      setPassword('');
+    }
+  };
+
   return (
     <form onSubmit={handleLogin}>
       <div>
         username
         <input
           value={username}
-          onChange={setUsername}
+          onChange={({ target }) => setUsername(target.value)}
           placeholder="Enter Username"
           name="username"
         />
@@ -20,7 +35,7 @@ function LoginForm({
         password
         <input
           value={password}
-          onChange={setPassword}
+          onChange={({ target }) => setPassword(target.value)}
           placeholder="Enter Password"
           name="password"
         />
@@ -30,13 +45,5 @@ function LoginForm({
     </form>
   );
 }
-
-LoginForm.propTypes = {
-  username: propTypes.string.isRequired,
-  password: propTypes.string.isRequired,
-  setUsername: propTypes.func.isRequired,
-  setPassword: propTypes.func.isRequired,
-  handleLogin: propTypes.func.isRequired,
-};
 
 export default LoginForm;
