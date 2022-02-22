f42444a96c09e0fd8a69c9c1fae613ba532da804
stivencabrera3 <stiven.cabrera3@gmail.com>
2022-02-22 01:38:25 -0500

    add api call function for fetching single user

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -7,12 +7,22 @@ const getAll = async () => {
     const response = await axios.get(baseUrl);
     return response.data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
+  }
+};
+
+const getOne = async (id) => {
+  try {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
 };
 
 const exports = {
   getAll,
+  getOne,
 };
 
 export default exports;
